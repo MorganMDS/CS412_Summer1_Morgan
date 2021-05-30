69e6d23c9aadcc9a07beea4404ca60fc597a2526
MorganMDS <1159791920@qq.com>
2021-05-31 02:34:41 +0800

    ps2

---
+++ Problem2.js
@@ -0,0 +1,21 @@
+function evaluate(expression) {
+    var num1 = parseInt(expression.charAt(0));
+    var num2 = parseInt(expression.charAt(2));
+    var operator = expression.charAt(1);
+    switch (operator) {
+        case '+':
+            return str => num1 + num2;
+        case '-':
+            return str => num1 - num2;
+        case '*':
+            return str => num1 * num2;
+        case '/':
+            return str => num1 / num2;
+        case '^':
+            return str => Math.pow(num1, num2);
+    }
+}
+
+expression = '2^4';
+let operator = evaluate(expression);
+console.log(`${expression} = ${operator(expression)}`);
