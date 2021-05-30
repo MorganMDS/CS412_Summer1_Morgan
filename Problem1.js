fe1f0dcf66dfe776ef216257df768c176243af21
MorganMDS <1159791920@qq.com>
2021-05-31 01:42:22 +0800

    ps1

--- Problem1.js
+++ Problem1.js
@@ -1,2 +1,5 @@
-ssdasd
-sdasdasdasdasd
+// Problem1:
+
+const str_sort = str =>str.split('').sort().join('')
+var str = 'supercalifragilisticexpialidocious';
+console.log(`${str} -> ${str_sort(str)}`)
